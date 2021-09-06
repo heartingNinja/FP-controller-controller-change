fcc3bcd5fbc5d84e66e660d0fbe9d4e5ff560eaa
heartingNinja <90154116+heartingNinja@users.noreply.github.com>
2021-09-07 03:52:00 +0700

    Update PlayerControllerFP.cs

--- PlayerControllerFP.cs
+++ PlayerControllerFP.cs
@@ -2,9 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace NF
-{
-
     // basic controller to work with animations. Still need to add jump
     public class PlayerControllerFP : MonoBehaviour
     {
@@ -147,6 +144,3 @@ namespace NF
        
 
         }
-    }
-
-
